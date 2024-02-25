a5789224baab0338786aef638331216877ea5d9c
José Alcalá <jose.alcala@potros.itson.edu.mx>
2024-02-25 13:07:19 -0800

    Creación del repositorio del proyecto.

---
+++ Rummy/src/main/java/com/itson/rummy/Rummy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itson.rummy;
+
+/**
+ *
+ * @author Yeisi
+ */
+public class Rummy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
