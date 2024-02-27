420687a8807838c462f477d615b8948276839e7f
GerardoZillmann <gerardozillmann1@gmail.com>
2024-02-26 22:46:19 -0700

    Prueba
    Prueba PUSH

--- Rummy/src/main/java/com/itson/rummy/Rummy.java
+++ Rummy/src/main/java/com/itson/rummy/Rummy.java
@@ -11,6 +11,6 @@ package com.itson.rummy;
 public class Rummy {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ke ondas!");
     }
 }
